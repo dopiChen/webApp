3be76e1c4446dda912f4e596121f179304506b5b
来应川 <2469871958@qq.com>
2024-07-15 17:14:18 +0800

    third2分页

--- src/api/user.js
+++ src/api/user.js
@@ -4,6 +4,7 @@ export const _getUserInfo = (errMsg) => http.GET('/api/auth/userInfo', {}, errMs
 // 登出
 export const userLogout = () => http.GET('/api/whitelistSetting/logout')
 // 分页查询用户
-export const userList = (obj) => http.GET('/api/whitelistSetting/pageList', obj)
+export const batchList = (obj) => http.GET('/api/batch/pageList', obj)
+export const batchSearch = (obj) => http.GET(`/searchBatch${obj}`, obj)
 
 export const _login = (account) => http.POST('/api/user/login', account)
